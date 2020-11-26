e9f38e1527605878d61360d895992d77b5c31eff
Nicolas DEBEURME <nicolas.debeurme@student.yncrea.fr>
2020-11-26 20:17:51 +0100

    FIN TP5

--- TP5/Exo2/Exo2.c
+++ TP5/Exo2/Exo2.c
@@ -4,20 +4,38 @@
 #include <math.h>
 #include "Exo2.h"
 #define incrsizetab 50 //valeur d'incrémentation (taille tableau)
-#define pos 50  //position élément
+#define pos 160  //position élément a modifier
 #define element 16//element a ajouter
+
 int main() {
 	TABLEAU tableau;
+	tableau.elt = NULL;
+	tableau.size = 0;
+	tableau.eltsCount = 0;
 
-	NewArray(tableau);
+	tableau=NewArray(tableau);
+
+	for (int i = 0; i < tableau.size; i++)
+	{
+		*(tableau.elt + i) = i;
+	}
+	displayElements(&tableau, 1, tableau.size);
 
 	incrementArraySize(&tableau, incrsizetab);
+	printf("\nLe tableau est maintenant compose de %d cases. \n ", tableau.size);
+
+	displayElements(&tableau, 1, tableau.size);
 
 	setElement(&tableau, pos, element);
+	printf("\nLe tableau est maintenant compose de %d cases. \n ", tableau.size);
 
-	displayElements(&tableau, 20, 60);
-
+	displayElements(&tableau, 1, tableau.size);
 
 	deleteElements(&tableau, 20 , 60);
+	printf("\nLe tableau est maintenant compose de %d cases. \n ", tableau.size);
 
+	displayElements(&tableau, 1, tableau.size);
+
+	free(tableau.elt);
+	return(EXIT_SUCCESS);
 }
--- TP5/Exo2/Fctn.c
+++ TP5/Exo2/Fctn.c
@@ -25,7 +25,7 @@ TABLEAU NewArray(TABLEAU tab) {
 int incrementArraySize(TABLEAU* tab, int incrementValue)
 {
 		int* tabl = NULL;
-		tabl=(int*)realloc(tab->elt, tab->size + incrementValue * sizeof(int));
+		tabl=(int*)realloc(tab->elt, (tab->size + incrementValue )* sizeof(int));
 
 		if (tabl == NULL) {
 			printf("mémoire insuffisante");
@@ -34,6 +34,8 @@ int incrementArraySize(TABLEAU* tab, int incrementValue)
 
 		else {
 			tab->elt = tabl;
+			for (int i = tab->size; i < tab->size+incrementValue; i++)
+				*(tab->elt + i) = i;
 			tab->size = tab->size + incrementValue;
 			return  tab->size;
 		}
@@ -43,18 +45,18 @@ int setElement(TABLEAU* tab, int pos, int element)
 {
 	int taille = tab->size;
 	if (pos <= tab->size) {
-		*(tab->elt + pos+1) = element;
+		*(tab->elt + pos-1) = element;
 		tab->eltsCount = tab->eltsCount + 1;
 		return pos;
 	}
 	else {
-		if (incrementArraySize(&tab, pos - tab->size)== -1) //Verifie que la relocalisation se passe bien
-			return 0;
+		if (incrementArraySize(tab, pos - tab->size) == -1)
+			return 0; //Verifie que la relocalisation se passe bien
 		else {
-			for (int i = 0; i < (pos-1); i++)
-				*(tab->elt + taille + i)=0;
+			for (int i = taille; i < (pos - 1); i++)
+				*(tab->elt + i) = i;
 
-			*(tab->elt + taille + pos - 1) = element; //écrit l'élement
+			*(tab->elt + pos - 1) = element; //écrit l'élement
 			tab->eltsCount = tab->eltsCount + 1;
 			return pos;
 		}
@@ -63,49 +65,53 @@ int setElement(TABLEAU* tab, int pos, int element)
 
 int displayElements(TABLEAU* tab, int startPos, int endPos)
 {
+	printf("\n");
 	if (startPos > 0 && endPos <= tab->size) {
 		if (startPos >= endPos) {
+			printf("Affichage de l'element %d a l'element %d: ", startPos, endPos);
 			for (int i = startPos; i >= endPos; i--) {
 				printf("%d ", *(tab->elt + i - 1));
-				return 0;
 			}
+			printf("\n");
+			return 0;
 		}
 		else {
+			printf("Affichage de l'element %d a l'element %d: ", startPos, endPos);
 			for (int i = startPos; i <= endPos; i++) {
-				printf("%d", *(tab->elt + i - 1));
-				return 0;
+				printf(" %d ", *(tab->elt + i - 1));
 			}
+			printf("\n");
+			return 0;
 		}
 	}
-	else
-	return -1;
+	else {
+		printf("Erreur dans les valeurs rentrées.\n");
+		return -1;
+	}
 }
 
 int deleteElements(TABLEAU* tab, int startPos, int endPos)
 {
-	if (startPos > 0 && endPos <= tab->size) {
+	if (0<startPos < tab->size && 0< endPos <= tab->size) {
 		if (startPos > endPos) {
 			int ecart = startPos - endPos;
-			for (int i = endPos; i < tab->size-ecart; i++) {
+			for (int i = endPos; i < tab->size-ecart; i++) 
 				*(tab->elt +i-1)=*(tab->elt+i+ecart);
-				tab->size = tab->size - ecart - 1;
-				return tab->size;
-			}
+			tab->size = tab->size - ecart - 1;
+			return tab->size;
 		}
 		if (startPos < endPos) {
 			int ecart = endPos-startPos;
-			for (int i = startPos; i < tab->size - ecart; i++) {
+			for (int i = startPos; i < tab->size - ecart; i++) 
 				*(tab->elt + i - 1) = *(tab->elt + i + ecart);
-				tab->size = tab->size - ecart - 1;
-				return tab->size;
-			}
+			tab->size = tab->size - ecart - 1;
+			return tab->size;
 		}
 		if(startPos==endPos){
-			for (int i = startPos; i < tab->size; i++) {
+			for (int i = startPos; i < tab->size; i++) 
 				*(tab->elt + i - 1)=*(tab->elt+i);
-				tab->size = tab->size - 1;
-				return tab->size;
-			}
+			tab->size = tab->size - 1;
+			return tab->size;
 		}
 	}
 	else
